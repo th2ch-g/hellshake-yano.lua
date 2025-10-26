1d0e7d6c094d04614e0e3cd295711ae38bdc6a49
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-10-27 01:09:48 +0900

    rm unnecessary files

--- tests/word_context_config_test.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This test file has been disabled because ContextDetector was removed in process6 sub4 refactoring.
- * ContextDetector was found to be unused and was part of the 798-line reduction effort.
- */
-
-// No tests - ContextDetector has been removed
